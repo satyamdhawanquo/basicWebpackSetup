b2ac219df300ffe9e690e07cdc03b123cda77562
satyamdhawanquo <satyam.dhawan@quovantis.com>
2020-05-23 19:15:36 +0530

    basic webpack setup: it includes react babel eslint

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = (env) => {
+  return {
+    context: path.resolve(__dirname, "src"),
+    entry: "./index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "learning_webpack_bundle.js",
+    },
+    mode: "development",
+    devServer: {
+      contentBase: "./public",
+    },
+    devtool:  "source-map" ,
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+  };
+};
